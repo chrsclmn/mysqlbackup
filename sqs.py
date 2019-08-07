9d56ac9549c98566e0b95a3dea3803fef8258d45
Chris Coleman <chrsclmn@gmail.com>
2019-08-07 10:11:20 +0100

    sqs.py: WaitTimeSeconds=10

--- sqs.py
+++ sqs.py
@@ -12,11 +12,11 @@ sqs = boto3.resource('sqs')
 @click.option('--queue-url', required=True)
 def work(queue_url):
     queue = sqs.Queue(queue_url)
-    messages = queue.receive_messages(MaxNumberOfMessages=1)
-    while len(messages) > 0:
-        for message in messages:
-            mysqlbackup.backup(**json.loads(message.body))
-            message.delete()
+    messages = queue.receive_messages(MaxNumberOfMessages=1,
+                                      WaitTimeSeconds=10)
+    for message in messages:
+        mysqlbackup.backup(**json.loads(message.body))
+        message.delete()
 
 
 if __name__ == '__main__':
